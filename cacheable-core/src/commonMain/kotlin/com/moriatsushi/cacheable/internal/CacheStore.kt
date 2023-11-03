01ec8daeea9b1cdd2b959098e9b5e926fe062727
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-03 21:19:08 +0900

    Use internal visibility

--- cacheable-core/src/commonMain/kotlin/com/moriatsushi/cacheable/internal/CacheStore.kt
+++ cacheable-core/src/commonMain/kotlin/com/moriatsushi/cacheable/internal/CacheStore.kt
@@ -2,11 +2,8 @@ package com.moriatsushi.cacheable.internal
 
 /**
  * A cache store.
- *
- * **DO NOT USE THIS CLASS DIRECTLY.**
- * it is used by the compiler plugin.
  */
-class CacheStore {
+internal class CacheStore {
     private var cache: Any? = null
 
     fun cacheOrInvoke(): Int {
