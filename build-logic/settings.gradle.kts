53dc7d80edd6337a494513d0f83dae386c9a964c
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-04 09:52:46 +0900

    Add a build-logic module to use the gradle plugin

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,15 @@
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "build-logic"
+include(":chacheable-gradle-plugin")
+project(":chacheable-gradle-plugin").projectDir = File("../cacheable-gradle-plugin")
