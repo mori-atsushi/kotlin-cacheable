221d43c3996c21548241481bf9a24c964a4c2181
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-05 23:49:56 +0900

    Refactor

--- cacheable-compiler/src/main/kotlin/com/moriatsushi/cacheable/compiler/declaration/CacheableAnnotationDeclaration.kt
+++ cacheable-compiler/src/main/kotlin/com/moriatsushi/cacheable/compiler/declaration/CacheableAnnotationDeclaration.kt
@@ -4,9 +4,13 @@ import org.jetbrains.kotlin.ir.backend.js.utils.valueArguments
 import org.jetbrains.kotlin.ir.declarations.IrAnnotationContainer
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.util.getAnnotation
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.name.FqName
 
 class CacheableAnnotationDeclaration {
+    fun hasAnnotation(annotationContainer: IrAnnotationContainer): Boolean =
+        annotationContainer.hasAnnotation(name)
+
     fun findMaxCountExpression(annotationContainer: IrAnnotationContainer): IrExpression? =
         annotationContainer.getAnnotation(name)
             ?.valueArguments
